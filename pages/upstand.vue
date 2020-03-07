e30d511315a1c7d944851622f30148fe2922de37
Loan Lai <Loan@Loans-iMac.home>
2020-03-07 15:20:26 -0500

    First commit.

---
+++ pages/upstand.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="div">
+        <h1>UpStands Page</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae doloremque blanditiis at necessitatibus pariatur dolorum quam in exercitationem ratione possimus quis voluptate expedita mollitia quasi corporis ex, inventore temporibus fugit!
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="css" scoped>
+ h1 {
+     color: pink;
+ }
+</style>
